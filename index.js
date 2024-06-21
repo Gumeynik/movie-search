ea00ef104a46b8093861051f30ea66f559123959
Aleksandr Kubenko <151834458+Gumeynik@users.noreply.github.com>
2024-06-21 15:46:18 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const inputNode = document.querySelector('.input-js'); 
 const btnNode = document.querySelector('.btn-js'); 
 const result = document.querySelector('#result')
+const error = document.querySelector('.error')
 
 btnNode.addEventListener('click', search); 
 
@@ -11,24 +12,39 @@ function search() {
     fetch(`http://www.omdbapi.com/?s=${film}&apikey=d1a26e34`)
         .then(response => response.json())
         .then(json => {
-            result.innerHTML = ''; 
-            for (let i = 0; i < 3; i++) {
-                const movie = json.Search[i];
-                const movieElement = document.createElement('a');
-                movieElement.href = `movie.html?id=${movie.imdbID}&apikey=d1a26e34`;
-                movieElement.target = '_blank'; 
-                movieElement.classList.add('movie-list');
-                    movieElement.innerHTML = `
-                        <img src="${movie.Poster}" alt="${movie.Title}" class="poster"/>
-                        <div class="description">
-                        <h2 class="title">${movie.Title}</h2>
-                        <p class="year"> ${movie.Year}</p>
-                        <p class="type"> ${movie.Type}</p>
-                        </div>
-                    `;
-                    result.appendChild(movieElement);
-                    inputNode.value = '';
+            if (json.Response==='False') {
+                console.log(123)
+                result.innerHTML = '',
+                error.textContent = 'Фильмы не найдены',
+                inputNode.addEventListener('click', function() {
+                    this.value = ''; 
+                })
             }
+            else {
+                result.innerHTML = ''; 
+                error.textContent = '';
+                for (let i = 0; i < 3; i++) {
+                    const movie = json.Search[i];
+                    const movieElement = document.createElement('a');
+                    movieElement.href = `movie.html?id=${movie.imdbID}&apikey=d1a26e34`;
+                    movieElement.target = '_blank'; 
+                    movieElement.classList.add('movie-list');
+                        movieElement.innerHTML = `
+                            <img src="${movie.Poster}" alt="${movie.Title}" class="poster"/>
+                            <div class="description">
+                            <h2 class="title">${movie.Title}</h2>
+                            <p class="year"> ${movie.Year}</p>
+                            <p class="type"> ${movie.Type}</p>
+                            </div>
+                        `;
+                        result.appendChild(movieElement);
+                        inputNode.value = '';
+                }
+            }
+     
         })
+        
 }
 //{Title: 'Fast & Furious 6', Year: '2013', imdbID: 'tt1905041', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMTM3NTg2NDQzOF5BMl5BanBnXkFtZTcwNjc2NzQzOQ@@._V1_SX300.jpg'}
+// .catch(error => 
+//     console.log('123'),
--- movie.js
+++ movie.js
@@ -9,24 +9,41 @@ console.log(imdbID)
             <img id="film-poster" src="${movie.Poster}" alt="${movie.Title}">
             <div class="film-description">
             <h2 id="film-title">${movie.Title}</h2>
-            <p id="film-year">${movie.Year}</p>
-            <p id="film-rated">${movie.Rated}</p>
-            <p id="film-Released">${movie.Released}</p>
-            <p id="film-Runtime">${movie.Runtime}</p>
-            <p id="film-genre">${movie.Genre}</p>
-            <p id="film-director">${movie.Director}</p>
-            <p id="film-writer">${movie.Writer}</p>
-            <p id="film-actors">${movie.Actors}</p>
-            <p id="film-plot">${movie.Plot}</p>
+            <p id="film-year">Год: <span style="color: rgb(0, 179, 255);">${movie.Year}</span></p>
+            <p id="film-rated">Рейтинг: <span style="color: rgb(0, 179, 255);">${movie.Rated}</span></p>
+            <p id="film-Released">Дата выхода: <span style="color: rgb(0, 179, 255);>${movie.Released}</span></p>
+            <p id="film-Runtime">Продолжительность: <span style="color: rgb(0, 179, 255);">${movie.Runtime}</span></p>
+            <p id="film-genre">Жанр: <span style="color: rgb(0, 179, 255);">${movie.Genre}</span></p>
+            <p id="film-director">Режиссер: <span style="color: rgb(0, 179, 255);">${movie.Director}</span></p>
+            <p id="film-writer">Сценарий: <span style="color: rgb(0, 179, 255);">${movie.Writer}</span></p>
+            <p id="film-actors">Актеры: <span style="  color: rgb(0, 179, 255);">${movie.Actors}</span></p>
             </div>
             `;
+            document.getElementById('film-plot').innerHTML = `
+            <p id="film-plot">${movie.Plot}</p>
+        `;
         })
+
         .catch(error => console.error('Error fetching movie details:', error));
 };
+
 // ------------------
+
+
 // <img src="" alt="" class="poster"/>
 // <div class="description">
 // <h2 class="title">${movie.Title}</h2>
 // <p class="year"> ${movie.Year}</p>
 // <p class="type"> ${movie.Type}</p>
-// </div>
+// </div>            <img id="film-poster" src="${movie.Poster}" alt="${movie.Title}">
+// {/* <div class="film-description">
+// <h2 id="film-title">${movie.Title}</h2>
+// <p id="film-year">Год:<span style="color: blue;">${movie.Year}</span></p>
+// <p id="film-rated">Рейтинг:${movie.Rated}</p>
+// <p id="film-Released">Дата выхода:${movie.Released}</p>
+// <p id="film-Runtime">Продолжительность:${movie.Runtime}</p>
+// <p id="film-genre">Жанр:${movie.Genre}</p>
+// <p id="film-director">Режиссер: ${movie.Director}</p>
+// <p id="film-writer">Сценарий:${movie.Writer}</p>
+// <p id="film-actors">Актеры:${movie.Actors}</p>
+// <p id="film-plot">${movie.Plot}</p> */}
